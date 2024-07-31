a52e8c4e42d8d2795d48a8879fa43bc35bfac10a
ErickEstuardoGiron <ekarlluvi123@gmail.com>
2024-07-31 10:56:12 -0600

    Mi Nombre Erick

---
+++ src/main/java/com/erick/minombre/MinombreApplication.java
@@ -0,0 +1,13 @@
+package com.erick.minombre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinombreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinombreApplication.class, args);
+	}
+
+}
